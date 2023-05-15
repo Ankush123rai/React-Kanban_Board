9e60d2b8cc2af51a01f238115c43577c3d9613cd
Ankush123rai <ankush98rai@gamil.com>
2023-05-15 23:25:16 +0530

    background image

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,18 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store';
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
+ 
+
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
       <App />
+      </PersistGate>
     </Provider> 
   </React.StrictMode>,
 )
--- src/store.jsx
+++ src/store.jsx
@@ -1,10 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import listReducer from "./components/features/listSlice";
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+import { combineReducers } from '@reduxjs/toolkit';
+
+const persistConfig={
+  key: "root",
+  version:1,
+  storage
+}
+const reducer=combineReducers({
+  lists: listReducer
+})
+const persistedReducer=persistReducer(persistConfig, reducer)
 
 const store= configureStore({
-  reducer: {
-    lists: listReducer
-  }
+  reducer: persistedReducer
 });
 
 export default store;
