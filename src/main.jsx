0f116d265b803456f3b121db10cb3325841b3370
KamalPanwar <kamalpanwar193@gmail.com>
2023-05-15 23:44:57 +0530

    added description component and routing

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux'
 import store from './store';
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from 'redux-persist'
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
