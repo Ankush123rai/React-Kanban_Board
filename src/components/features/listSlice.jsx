ab90cb2d4d09c62667e1f65924d01f8cf95b66bf
Dilip <dilip.884400@gmail.com>
2023-05-16 23:51:52 +0530

    drag and drop functionality done in cards

--- src/components/features/listSlice.jsx
+++ src/components/features/listSlice.jsx
@@ -1,36 +1,67 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { v4 as uuid } from 'uuid';
+import { createSlice } from "@reduxjs/toolkit";
+import { v4 as uuid } from "uuid";
 
 const listSlice = createSlice({
-  name: 'lists',
+  name: "lists",
   initialState: {
-    lists: []
+    lists: [],
   },
-
   reducers: {
     addList: (state, action) => {
       const { title } = action.payload;
-      state.lists.push({ id: uuid(), title});
+      state.lists.push({ id: uuid(), title });
     },
-
     deleteList: (state, action) => {
-      state.lists = state.lists.filter((item) => item.id !== action.payload)
+      state.lists = state.lists.filter((item) => item.id !== action.payload);
     },
-
     addCard: (state, action) => {
-        state.lists.map((item) => {
-            if(item.id === action.payload.listId){
-                if(Object.hasOwn(item, 'task')){
-                    item.task.push(action.payload)
-                }else{
-                    item.task =[];
-                    item.task.push(action.payload)
-                }
-            }
-        })
-    }
-     
+      const { listId, title } = action.payload;
+      const list = state.lists.find((item) => item.id === listId);
+      if (list) {
+        if (!list.task) {
+          list.task = [];
+        }
+        list.task.push({ id: uuid(), title });
+      }
+    },
+    reorderLists: (state, action) => {
+      const { startIndex, endIndex } = action.payload;
+      const [removed] = state.lists.splice(startIndex, 1);
+      state.lists.splice(endIndex, 0, removed);
+    },
+    reorderCards: (state, action) => {
+      const { listId, startIndex, endIndex } = action.payload;
+      const list = state.lists.find((list) => list.id === listId);
+      if (list) {
+        const [removed] = list.task.splice(startIndex, 1);
+        list.task.splice(endIndex, 0, removed);
+      }
+    },
+    moveCardAcrossLists: (state, action) => {
+      const { sourceListId, destListId, sourceIndex, destIndex, cardId } =
+        action.payload;
+      const sourceList = state.lists.find((list) => list.id === sourceListId);
+      const destList = state.lists.find((list) => list.id === destListId);
+
+      if (sourceList && destList) {
+        const [removed] = sourceList.task.splice(sourceIndex, 1);
+        const card = {
+          id: cardId,
+          title: removed.title,
+        };
+        destList.task.splice(destIndex, 0, card);
+      }
+    },
   },
 });
-export const { addList, deleteList, addCard } = listSlice.actions;
+
+export const {
+  addList,
+  deleteList,
+  addCard,
+  reorderLists,
+  reorderCards,
+  moveCardAcrossLists,
+} = listSlice.actions;
+
 export default listSlice.reducer;
--- src/components/mainCard/MainCard.jsx
+++ src/components/mainCard/MainCard.jsx
@@ -1,57 +1,134 @@
-import React, { useState } from "react";
-import style from "./MainCard.module.css";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {deleteList } from '../features/listSlice';
+import {
+  deleteList,
+  reorderLists,
+  reorderCards,
+  moveCardAcrossLists,
+} from "../features/listSlice";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import style from "./MainCard.module.css";
 import InputCard from "../inputCard/InputCard";
-import Card from '../../atoms/card/card';
-import { Link } from 'react-router-dom'
-
-
+import Card from "../../atoms/card/card";
+import { Link } from "react-router-dom";
 
 function MainCard() {
   const dispatch = useDispatch();
   const reduxData = useSelector((state) => state.lists.lists);
   console.log(reduxData);
- 
 
+  const handleDragEnd = (result) => {
+    if (!result.destination) return;
 
-  
+    const { source, destination, draggableId, type } = result;
+
+    if (type === "list") {
+      dispatch(reorderLists(source.index, destination.index));
+    } else if (type === "card") {
+      const sourceListId = source.droppableId;
+      const destListId = destination.droppableId;
+
+      if (sourceListId === destListId) {
+        dispatch(
+          reorderCards({
+            listId: sourceListId,
+            startIndex: source.index,
+            endIndex: destination.index,
+          })
+        );
+      } else {
+        dispatch(
+          moveCardAcrossLists({
+            sourceListId,
+            destListId,
+            sourceIndex: source.index,
+            destIndex: destination.index,
+            cardId: draggableId,
+          })
+        );
+      }
+    }
+  };
+
   return (
-    
-    <div className={style.container}>
-      <div className={style.lists_container}>
-        {reduxData.map((list) => (
-          <div className={style.list_container} key={list.id}>
-            <div className={style.list_title}>
-              <h3>{list.title}</h3>
-              <button
-                className={style.delete_button}
-                onClick={() => dispatch(deleteList(list.id))}
-              >
-                X
-              </button>
-              </div>
-             <div className={style.card_container}>
-             {list.task &&
-                    list.task.map((task) => (
-                      <div key={task.id} className={style.card}>
-                      <Link style={{color: "inherit",textDecoration:'none'}} to={`description/:${task.title}`}><Card title={task.title}/></Link>  
-                        {/* <p>{task.title}</p> */}
-                        
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <div className={style.container}>
+        <Droppable droppableId="list-container" type="list">
+          {(provided) => (
+            <div
+              className={style.lists_container}
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {reduxData.map((list, index) => (
+                <Draggable key={list.id} draggableId={list.id} index={index}>
+                  {(provided) => (
+                    <div
+                      className={style.list_container}
+                      {...provided.draggableProps}
+                      ref={provided.innerRef}
+                    >
+                      <div className={style.list_title}>
+                        <h3 {...provided.dragHandleProps}>{list.title}</h3>
+                        <button
+                          className={style.delete_button}
+                          onClick={() => dispatch(deleteList(list.id))}
+                        >
+                          X
+                        </button>
                       </div>
-                    ))}
-             </div>
-
-            <div className={style.input_container}>
-              <InputCard listId={list.id} type="card"/>
+                      <Droppable droppableId={list.id} type="card">
+                        {(provided) => (
+                          <div
+                            className={style.card_container}
+                            {...provided.droppableProps}
+                            ref={provided.innerRef}
+                          >
+                            {list.task &&
+                              list.task.map((task, index) => (
+                                <Draggable
+                                  key={task.id}
+                                  draggableId={task.id}
+                                  index={index}
+                                >
+                                  {(provided) => (
+                                    <div
+                                      className={style.card}
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}
+                                      ref={provided.innerRef}
+                                    >
+                                      <Link
+                                        style={{
+                                          color: "inherit",
+                                          textDecoration: "none",
+                                        }}
+                                        to={`description/:${task.title}`}
+                                      >
+                                        <Card title={task.title} />
+                                      </Link>
+                                    </div>
+                                  )}
+                                </Draggable>
+                              ))}
+                            {provided.placeholder}
+                          </div>
+                        )}
+                      </Droppable>
+                      <div className={style.input_container}>
+                        <InputCard listId={list.id} type="card" />
+                      </div>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
             </div>
-
-           </div>
-
-        ))}
+          )}
+        </Droppable>
+        <InputCard />
       </div>
-            <InputCard/>
-    </div>
+    </DragDropContext>
   );
 }
 
