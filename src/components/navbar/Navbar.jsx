67328ba0c7b8fda8759f4286ea82319db3c21153
Dilip <dilip.884400@gmail.com>
2023-05-16 16:23:01 +0530

    NavBar Added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import style from './Navbar.module.css'
+import React from "react";
+import style from "./Navbar.module.css";
+import { BiSearchAlt2 } from "react-icons/Bi";
+import { IoIosNotifications } from "react-icons/Io";
+import { GrCircleInformation } from "react-icons/Gr";
 
 const Navbar = () => {
   return (
     <div className={style.navbar}>
-      <h2>Home Task Management</h2>
-    </div>
-  )
-}
+      <div className={style.navbarLeft}>
+        <img
+          className={style.navbarImg}
+          src="https://public-files.gumroad.com/f7o3inoeloo0cxh572go242z2q4v"
+        />
+        <h2 className={style.navbarItem}>KanBan</h2>
+        <h3 className={style.navbarItem}>WorkSpaces</h3>
+        <h3 className={style.navbarItem}>Recent</h3>
+        <h3 className={style.navbarItem}>Stared</h3>
+        <h3 className={style.navbarItem}>Tamplets</h3>
+        <button className={style.navbarBtn}>Create</button>
+      </div>
 
-export default Navbar
+      <div className={style.navbarRight}>
+        
+          <input className={style.navbarSearch} type="text" placeholder="Search" />
+      
+        <BiSearchAlt2 className={style.Icon} />
+        <IoIosNotifications className={style.Icon} />
+        <GrCircleInformation className={style.Icon} />
+        <img
+          className={style.userImage}
+          src="https://img.freepik.com/premium-psd/3d-cartoon-character-avatar-isolated-3d-rendering_235528-554.jpg?w=2000"
+          alt="user"
+          width="50px"
+          height="50px"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
